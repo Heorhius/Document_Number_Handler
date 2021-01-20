f156ea147287413cfd53a174fedf3e513f1d587b
Heorhi <heorhi.kaptsiug@gmail.com>
2021-01-21 02:22:56 +0300

    refactoring Main

--- src/com/kaptsiug/study/project/controller/Main.java
+++ src/com/kaptsiug/study/project/controller/Main.java
@@ -10,11 +10,14 @@ import com.kaptsiug.study.project.util.reader.ReaderType;
 
 public class Main {
 
+    public static final String MSG_NOT_VALIDATE_VALUE = "Couldn't validate value: %s\n";
+    public static final String DEFAULT_FILE = "test.txt";
+
     public static void main(String[] args) {
 
         ReaderCreator readerCreator = new ReaderCreator();
         IReadable reader = readerCreator.createReader(ReaderType.FILE);
-        String result = reader.read("test.txt");
+        String result = reader.read(DEFAULT_FILE);
         String[] parseResults = Parser.parseDocument(result);
 
         if (parseResults.length != 0) {
@@ -33,7 +36,7 @@ public class Main {
 
                     System.out.println(DocumentHandler.getLettersFromDocNumber(parseResults[i]));
 
-                } else System.out.printf("Couldn't validate value: %s\n", parseResults[i]);
+                } else System.out.printf(MSG_NOT_VALIDATE_VALUE, parseResults[i]);
             }
         }
 
