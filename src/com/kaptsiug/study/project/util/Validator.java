4f3ce7a0317f938395a1716a6eddc8970092162c
Heorhi <heorhi.kaptsiug@gmail.com>
2021-01-21 02:15:55 +0300

    add solution

--- src/com/kaptsiug/study/project/util/Validator.java
+++ src/com/kaptsiug/study/project/util/Validator.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 
 public class Validator {
     public static boolean validateDocNumber(String value) {
-        //Pattern pattern = Pattern.compile("^[A-Z]{4}-\\d{3}-[A-Z]{4}-\\d{3}-([A-Z]\\d){2}$");
         Pattern pattern = Pattern.compile("^(\\d{4}-[A-Z]{3}-){2}(\\d[A-Z]){2}$");
         Matcher matcher = pattern.matcher(value);
         return matcher.matches();
